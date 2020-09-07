20b28877cebd59ac79e2bd8ffb149639b71a6051
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 02:55:47 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:55:47 up 1 day, 19:11,  1 user,  load average: 0.87, 0.98, 0.64 aba376f15e004446a28fcb6f834c3f6296f945c9

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -99,14 +99,14 @@ static bool make_token(char *e) {
 					}
 					case NUM: {
 						tokens[nr_token].type = rules[i].token_type;
-						strncpy(tokens[nr_token].str , substr_start+1 , substr_len);
+						strncpy(tokens[nr_token].str , substr_start , substr_len);
 						tokens[nr_token].str[substr_len] = '\0';
 						nr_token ++;
 						break;
 					}
 					case HENUM: {
 						tokens[nr_token].type = rules[i].token_type;
-						strncpy(tokens[nr_token].str , substr_start+1 , substr_len);
+						strncpy(tokens[nr_token].str , substr_start , substr_len);
 						tokens[nr_token].str[substr_len] = '\0';
 						nr_token ++;
 						break;
