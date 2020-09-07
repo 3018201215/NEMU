b9f31cb7efc29adcbb7500513c9f35119681480f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 04:41:11 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:41:11 up 1 day, 20:56,  1 user,  load average: 0.82, 0.68, 0.70 544fb89746d03aaf102d8db5f0f0b860558b7a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -225,7 +225,7 @@ uint32_t eval(int p, int q){
 				op = i;
 			}
 		}
-
+		printf("a %d %d %d\n", p,op,q);
 		if( p == op || tokens[op].type == M || tokens[op].type == P || tokens[op].type == NO){
 			uint32_t val = eval(op+1 , q);
 			switch(tokens[op].type){
@@ -240,9 +240,9 @@ uint32_t eval(int p, int q){
 		uint32_t val2 = eval(op+1 , q);
 		printf("Val: %u     %u\n", val1, val2);
 		switch(tokens[op].type){
-			case '+': {printf("asdsadasdsad\n"); return val1 + val2; printf("wwqqwqq\n");}
+			case '+': return val1 + val2;
 			case '-': return val1 - val2;
-			case '*': {printf("wwwwww\n"); return val1 * val2;}
+			case '*': return val1 * val2;
 			case '/': return val1 / val2;
 			case OR: return val1 || val2;
 			case AND: return val1 && val2;
