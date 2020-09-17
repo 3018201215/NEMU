48a01581d541e35fa69793697e1ff95720f6a39b
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-17 12:25:59 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:25:59 up 5 days,  3:40,  1 user,  load average: 0.87, 0.68, 0.63 749201311769bb9e3de66ec0462c7ac33e0ceb9

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -2,6 +2,7 @@
 
 #include "data-mov/mov.h"
 #include "data-mov/xchg.h"
+#include "data-mov/push.h"
 
 #include "arith/dec.h"
 #include "arith/inc.h"
@@ -25,3 +26,5 @@
 #include "misc/misc.h"
 
 #include "special/special.h"
+
+#include "call/call.h"
---
+++ nemu/src/cpu/exec/call/call-template.h
@@ -0,0 +1,27 @@
+#include "cpu/exec/template-start.h"
+
+#define instr call
+
+
+
+make_helper(concat(call_rm_, SUFFIX)) {
+	int len = concat(decode_rm_, SUFFIX)(eip + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	swaddr_write(reg_l(R_ESP), 4, cpu.eip + len);
+	DATA_TYPE_S addr = op_src->val;
+	print_asm("call 0x%X", addr);
+	cpu.eip = addr - len - 1;
+	return len+1;
+}
+
+make_helper(concat(call_i_, SUFFIX)){
+	int len = concat(decode_i_, SUFFIX)(eip + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	swaddr_write(reg_l(R_ESP), 4, cpu.eip + len);
+	DATA_TYPE_S addr = op_src->val;
+	print_asm("call 0x%X", cpu.eip+1+len+addr);
+	cpu.eip += addr;
+	return len+1;
+}
+
+#include "cpu/exec/template-end.h"
