91355f16801bf68b102057e0fabf9544e97a98fd
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-20 02:51:00 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:51:00 up 7 days, 18:05,  1 user,  load average: 0.40, 4.94, 5.33 ba894caca7aa2da5c3500c885d4e1f73a3b19e0

--- nemu/src/cpu/exec/test/test-template.h
+++ nemu/src/cpu/exec/test/test-template.h
@@ -3,7 +3,7 @@
 #define instr test
 
 make_helper(concat(test_r_, SUFFIX)) {
-	swaddr_t val = op_src->val;
+	swaddr_t val = op_src2->val;
 	uint32_t result = val && val;
 	if(result == 0 ) cpu.EFLAGS = 0x00000042;
 	print_asm("test" str(SUFFIX) " value: 0x%X, result: %d", val, result);
