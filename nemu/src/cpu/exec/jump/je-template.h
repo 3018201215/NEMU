457bf305259fa64915cee7211e3f8029b712a547
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-21 09:16:53 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:16:53 up 9 days, 31 min,  1 user,  load average: 0.74, 0.76, 0.70 fe9dd0b3fb8ba39e6b1a0ff3a9c8151a282f30b4

--- nemu/src/cpu/exec/jump/je-template.h
+++ nemu/src/cpu/exec/jump/je-template.h
@@ -3,7 +3,7 @@
 #define instr je
 
 static void do_execute(){
-	uint32_t addr = op_src->val;
+	DATA_TYPE_S addr = op_src->val;
 	print_asm("jump 0x%X  0x%X  %d", cpu.eip+1+DATA_BYTE+addr, addr, cpu.ZF);
 	if(cpu.ZF == 1) cpu.eip += addr;
 }
