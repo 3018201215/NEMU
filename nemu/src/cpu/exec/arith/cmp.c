6d27f33b00657c7ed695195464cbadab144aecb3
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-21 14:42:13 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:42:13 up 9 days,  5:56,  1 user,  load average: 0.86, 0.69, 0.63 51e07130aab93cfc207b1552f1c1f5135bab9368

--- nemu/src/cpu/exec/arith/cmp.c
+++ nemu/src/cpu/exec/arith/cmp.c
@@ -15,4 +15,5 @@
 /* for instruction encoding overloading */
 
 make_helper_v(cmp_r2rm)
+make_helper_v(cmp_i2rm)
 
--- nemu/src/cpu/exec/arith/cmp.h
+++ nemu/src/cpu/exec/arith/cmp.h
@@ -3,5 +3,7 @@
 
 make_helper(cmp_r2rm_b);
 make_helper(cmp_r2rm_v);
+make_helper(cmp_i2rm_v);
+make_helper(cmp_i2rm_b);
 
 #endif
