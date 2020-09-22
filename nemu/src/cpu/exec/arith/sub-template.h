3d5b341c36e9854c160facc891be89e6021bc515
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-22 11:43:06 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:43:06 up 10 days,  2:57,  1 user,  load average: 0.90, 0.70, 0.73 94c0c3251a277a8853b7ebd21efe08c6433bb56e

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -4,11 +4,12 @@
 
 static void do_execute() {
 	DATA_TYPE result = op_dest->val - op_src->val;
-	//op_dest->val = result;
 	int len = (DATA_BYTE << 3) - 1;
-	cpu.CF = 0;
-	cpu.OF = 0;
+	cpu.CF = op_dest->val < op_src->val;
+	int a = op_src->val >> len;
+	int b = op_dest->val >> len;
 	cpu.SF = result >> len;
+	cpu.OF = (a != b && a == cpu.SF);
 	uint32_t s = result;
 	int i;
 	for(i=1; i<8; i++){
@@ -18,6 +19,7 @@ static void do_execute() {
 	if(result == 0){
 		cpu.ZF = 1;
 	}else cpu.ZF = 0;
+	//op_dest->val = result;
 	print_asm("sub" str(SUFFIX) " val1: 0x%X, val2: 0x%X, result: 0x%X", op_src->val, op_dest->val, result);
 }
 
