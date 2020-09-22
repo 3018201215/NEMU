f0ee0d074220166ce68a9fb7fbfd67ec829b00c2
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-22 11:25:37 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:25:37 up 10 days,  2:40,  1 user,  load average: 1.13, 0.94, 0.90 5401672868cdb94befc1140c12835a5d3b02d6e

---
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -0,0 +1,29 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sub
+
+static void do_execute() {
+	if(DATA_BYTE == 1) op_src->val = (int8_t) op_src->val;
+	uint32_t result = op_dest->val - op_src->val;
+	op_dest->val = result;
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF = 0;
+	cpu.OF = 0;
+	cpu.SF = result >> len;
+	uint32_t s = result;
+	int i;
+	for(i=1; i<8; i++){
+		s ^= result >> i;
+	}
+	cpu.PF = !(s && 1);
+	if(result == 0){
+		cpu.ZF = 1;
+	}else cpu.ZF = 0;
+	print_asm_template2();
+}
+
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+
+
+#include "cpu/exec/template-end.h"
