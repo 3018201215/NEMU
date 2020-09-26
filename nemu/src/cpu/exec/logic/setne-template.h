307150affd7a06578b58ff9ff9ab8cfda80a6916
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 05:01:38 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:01:38 up 21:06,  1 user,  load average: 0.52, 0.71, 0.68 26d68f844aff2cdd3846a67de7ac7f2aabfc40f0

---
+++ nemu/src/cpu/exec/logic/setne-template.h
@@ -0,0 +1,11 @@
+#include "cpu/exec/template-start.h"
+
+#define instr setne
+
+make_helper(concat(setne_r_, SUFFIX)){
+	if(cpu.ZF == 0) op_src->val = 0;
+	print_asm_template1();
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/logic/setne.h
@@ -0,0 +1,6 @@
+#ifndef __SETNE_H__
+#define __SETNE_H__
+
+make_helper(setne_r_b);
+
+#endif
