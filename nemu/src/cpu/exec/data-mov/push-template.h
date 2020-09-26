1394f20b7b7a734cf1197665bee737689b34f4cd
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 04:47:46 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:47:46 up 20:53,  1 user,  load average: 0.87, 0.82, 0.65 c50ad13a810cd644c4f51bfc9c81b251287b3636

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -6,7 +6,7 @@ static void do_execute(){
 	if(DATA_BYTE == 1) op_src->val = (int8_t)op_src->val;
 	reg_l(R_ESP) -= 4;
 	swaddr_write(reg_l(R_ESP), 4, op_src->val);
-	print_asm_template1();
+	print_asm("0x%X  0x%X",cpu.esp,cpu.ebp);
 }
 
 make_instr_helper(r)
