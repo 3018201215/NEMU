67d81f207ceadd86c044368b16c86e387a2a5ee5
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 03:56:06 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:56:06 up 20:01,  1 user,  load average: 0.72, 0.70, 0.68 dc6436d078988881e2b48e9847edfdbc8b22fc55

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -4,7 +4,7 @@
 #include "data-mov/xchg.h"
 #include "data-mov/push.h"
 #include "data-mov/pop.h"
-#include "data-mov/leave.h"
+//#include "data-mov/leave.h"
 
 #include "arith/dec.h"
 #include "arith/inc.h"
