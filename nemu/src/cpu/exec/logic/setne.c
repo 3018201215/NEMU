089e792348e88793f1e42c524a5f6c7036cf8863
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 05:04:38 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:04:38 up 21:09,  1 user,  load average: 0.89, 0.80, 0.72 395c4585f73e9515264e2272e11d3f6de6041bdf

--- nemu/src/cpu/exec/logic/setne.c
+++ nemu/src/cpu/exec/logic/setne.c
@@ -4,14 +4,14 @@
 #include "setne-template.h"
 #undef DATA_BYTE
 
-#define DATA_BYTE 2
-#include "setne-template.h"
-#undef DATA_BYTE
+// #define DATA_BYTE 2
+// #include "setne-template.h"
+// #undef DATA_BYTE
 
-#define DATA_BYTE 4
-#include "setne-template.h"
-#undef DATA_BYTE
+// #define DATA_BYTE 4
+// #include "setne-template.h"
+// #undef DATA_BYTE
 
 /* for instruction encoding overloading */
 
-make_helper_v(setne_r)
+// make_helper_v(setne_r)
