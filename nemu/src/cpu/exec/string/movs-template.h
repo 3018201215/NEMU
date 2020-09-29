ce12883409219e89f05e76346e2ed4641f96dd03
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 14:20:31 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:20:31 up 4 days,  6:25,  1 user,  load average: 0.92, 0.93, 0.72 957e7f0b31e0ee24a76821c0ae013948e9812a48

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -5,8 +5,7 @@
 make_helper(concat(movs_, SUFFIX)) {
 	if(ops_decoded.is_operand_size_16){
 		DATA_TYPE val1 = reg_w(R_SI);
-		DATA_TYPE val3 = swaddr_read(val1, 4);
-		swaddr_write(reg_w(R_DI), DATA_BYTE, val3);
+		swaddr_write(reg_w(R_DI), DATA_BYTE, swaddr_read(val1, 4));
 		if(cpu.DF == 0){
 			reg_w(R_DI) += DATA_BYTE;
 			reg_w(R_SI) += DATA_BYTE;
@@ -16,8 +15,7 @@ make_helper(concat(movs_, SUFFIX)) {
 		}
 	}else{
 		DATA_TYPE val1 = reg_l(R_ESI);
-		DATA_TYPE val3 = swaddr_read(val1, 4);
-		swaddr_write(reg_w(R_EDI), DATA_BYTE, val3);
+		swaddr_write(reg_w(R_EDI), DATA_BYTE, swaddr_read(val1, 4));
 		if(cpu.DF == 0){
 			reg_l(R_EDI) += DATA_BYTE;
 			reg_l(R_ESI) += DATA_BYTE;
