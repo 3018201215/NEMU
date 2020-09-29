d49cf9b3249acfc94547ce9d9133a8cb246620b6
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 14:26:50 +0000

    >  run 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:26:50 up 4 days,  6:32,  1 user,  load average: 1.15, 0.98, 0.80 12b7657bc96c0a6d988ce312a38aa5a9ffbff121

--- testcase/src/struct.c
+++ testcase/src/struct.c
@@ -4,7 +4,7 @@
 
 struct dummy {
 	int pad1[N];
-	//char pad2[N];
+	char pad2[N];
 } d;
 
 struct dummy fun(struct dummy a) {
@@ -15,14 +15,14 @@ int main() {
 	int i;
 	for(i = 0; i < N; i ++) {
 		d.pad1[i] = i + 128;
-		//d.pad2[i] = i;
+		d.pad2[i] = i;
 	}
 
 	struct dummy t = fun(d);
 
 	for(i = 0; i < N; i ++) {
 		nemu_assert(t.pad1[i] == i + 128);
-		//nemu_assert(t.pad2[i] == i);
+		nemu_assert(t.pad2[i] == i);
 	}
 
 	nemu_assert(i == N);
