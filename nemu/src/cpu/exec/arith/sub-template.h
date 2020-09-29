1fb3c9375a9d9bfcfbc88f0a3893302814a86967
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 13:21:42 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:21:42 up 4 days,  5:27,  1 user,  load average: 1.93, 1.06, 0.83 20b6158e5bbd00fc4fcec0f12736d2231b61f60

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -3,6 +3,7 @@
 #define instr sub
 
 static void do_execute() {
+	DATA_TYPE val1 = op_dest->val;
 	DATA_TYPE result = op_dest->val - op_src->val;
 	int len = (DATA_BYTE << 3) - 1;
 	cpu.CF = op_dest->val < op_src->val;
@@ -22,7 +23,7 @@ static void do_execute() {
 	OPERAND_W(op_dest, result);
 	//swaddr_write(op_dest->addr, DATA_BYTE, result);
 	//print_asm_template2();
-	print_asm("sub" str(SUFFIX) " value1: 0x%X, value2: 0x%X, result: %d", op_src->val, op_dest->val, result);
+	print_asm("sub" str(SUFFIX) " value1: 0x%X, value2: 0x%X, result: %d 0x%X", op_src->val, op_dest->val, result, val1);
 }
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(si2rm)
