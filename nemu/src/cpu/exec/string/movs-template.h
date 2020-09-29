e4908dd1d42e156febc2946ad264c8fd2f93f7f7
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 15:04:59 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:04:59 up 4 days,  7:10,  1 user,  load average: 0.79, 0.72, 0.66 3b93bdf37f72dc2259850ecd5f48b34176db77eb

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -4,8 +4,8 @@
 
 make_helper(concat(movs_, SUFFIX)) {
 	if(ops_decoded.is_operand_size_16){
-		DATA_TYPE val1 = reg_w(R_SI);
-		swaddr_write(reg_w(R_DI), DATA_BYTE, swaddr_read(val1, 4));
+		//DATA_TYPE val1 = reg_w(R_SI);
+		swaddr_write(reg_w(R_DI), DATA_BYTE, swaddr_read(reg_w(R_SI), 4));
 		if(cpu.DF == 0){
 			reg_w(R_DI) += DATA_BYTE;
 			reg_w(R_SI) += DATA_BYTE;
@@ -13,10 +13,10 @@ make_helper(concat(movs_, SUFFIX)) {
 			reg_w(R_DI) -= DATA_BYTE;
 			reg_w(R_SI) -= DATA_BYTE;
 		}
-		print_asm("movs" str(SUFFIX) "si:0x%X, di:0x%X", swaddr_read(val1, 4), reg_w(R_DI));
+		print_asm("movs" str(SUFFIX) "si:0x%X, di:0x%X", reg_w(R_SI), reg_w(R_DI));
 	}else{
 		DATA_TYPE val1 = reg_l(R_ESI);
-		swaddr_write(reg_w(R_EDI), DATA_BYTE, swaddr_read(val1, 4));
+		swaddr_write(reg_l(R_EDI), DATA_BYTE, swaddr_read(reg_l(R_ESI), 4));
 		if(cpu.DF == 0){
 			reg_l(R_EDI) += DATA_BYTE;
 			reg_l(R_ESI) += DATA_BYTE;
