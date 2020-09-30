3f0a19662f5b298c4255b90f3e269eb4e5f5c097
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 02:39:52 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:39:52 up 4 days, 18:45,  1 user,  load average: 0.94, 0.88, 0.74 49aead196ea4794adcf5ae738a0c1a2a312437a

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -5,7 +5,7 @@
 make_helper(concat(lods_, SUFFIX)) {
 	if(ops_decoded.is_operand_size_16){
 			if(DATA_BYTE == 1){
-				swaddr_write(REG(R_AL), DATA_BYTE, swaddr_read(reg_w(R_SI), 4));
+				swaddr_write(reg_b(R_AL), DATA_BYTE, swaddr_read(reg_w(R_SI), 4));
 				if(cpu.DF == 0){
 					reg_w(R_SI) += DATA_BYTE;
 				}else
