a9707d0479cb32f4b06cde207ae0d38495aa4d6f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 03:03:36 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:03:36 up 4 days, 19:08,  1 user,  load average: 1.00, 0.84, 0.76 47d4557ce8ffe5cbf3aa3ed31324cbc6fd9bd38

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -5,19 +5,19 @@
 make_helper(concat(lods_, SUFFIX)) {
 	if(ops_decoded.is_operand_size_16){
 			if(DATA_BYTE == 1){
-				REG(R_AL) = swaddr_read(reg_w(R_SI), DATA_BYTE);
+				reg_b(R_AL) = swaddr_read(reg_w(R_SI), DATA_BYTE);
 				if(cpu.DF == 0){
 					reg_w(R_SI) += DATA_BYTE;
 				}else
 					reg_w(R_SI) -= DATA_BYTE;
 			}else if(DATA_BYTE == 2){
-				swaddr_write(REG(R_AX), DATA_BYTE, swaddr_read(reg_w(R_SI), 4));
+				reg_w(R_AX) = swaddr_read(reg_w(R_SI), DATA_BYTE);
 				if(cpu.DF == 0){
 					reg_w(R_SI) += DATA_BYTE;
 				}else
 					reg_w(R_SI) -= DATA_BYTE;
 			}else{
-				swaddr_write(REG(R_EAX), DATA_BYTE, swaddr_read(reg_w(R_SI), 4));
+				reg_l(R_EAX) = swaddr_read(reg_w(R_SI), DATA_BYTE);
 				if(cpu.DF == 0){
 					reg_w(R_SI) += DATA_BYTE;
 				}else
@@ -25,26 +25,26 @@ make_helper(concat(lods_, SUFFIX)) {
 			}
 	}else{
 		if(DATA_BYTE == 1){
-				reg_l(R_AL) = swaddr_read(reg_w(R_SI), DATA_BYTE);
+				reg_b(R_AL) = swaddr_read(reg_w(R_SI), DATA_BYTE);
 				if(cpu.DF == 0){
 					reg_l(R_ESI) += DATA_BYTE;
 				}else
 					reg_l(R_ESI) -= DATA_BYTE;
 			}else if(DATA_BYTE == 2){
-				swaddr_write(REG(R_AX), DATA_BYTE, swaddr_read(reg_l(R_ESI), 4));
+				reg_w(R_AX) = swaddr_read(reg_w(R_SI), DATA_BYTE);
 				if(cpu.DF == 0){
 					reg_l(R_ESI) += DATA_BYTE;
 				}else
 					reg_l(R_ESI) -= DATA_BYTE;
 			}else{
-				swaddr_write(REG(R_EAX), DATA_BYTE, swaddr_read(reg_l(R_ESI), 4));
+				reg_l(R_EAX) = swaddr_read(reg_w(R_SI), DATA_BYTE);
 				if(cpu.DF == 0){
 					reg_l(R_ESI) += DATA_BYTE;
 				}else
 					reg_l(R_ESI) -= DATA_BYTE;
 			}
 	}
-	print_asm("lods" str(SUFFIX) "0x%X 0x%X",swaddr_read(reg_l(R_ESI), DATA_BYTE),reg_l(R_AL));
+	print_asm("lods" str(SUFFIX) "0x%X 0x%X",swaddr_read(reg_l(R_ESI), DATA_BYTE),reg_b(R_AL));
 	//print_asm("mov 0x%X 0x%X", op_src->val, cpu.eax);
 	return 1;
 }
