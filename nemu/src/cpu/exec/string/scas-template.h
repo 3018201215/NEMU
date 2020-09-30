3d72556d8e48b73cee81687a8594cc3679ba51c4
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 02:48:15 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:48:15 up 4 days, 18:53,  1 user,  load average: 1.09, 0.78, 0.73 c3afdc9e37fa8da37fe19f5a40a1845c648c48a1

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -5,46 +5,46 @@
 make_helper(concat(scas_, SUFFIX)) {
 	if(ops_decoded.is_operand_size_16){
 			if(DATA_BYTE == 1){
-				REG(R_AL) = swaddr_read(reg_w(R_SI), DATA_BYTE);
+				REG(R_AL) = swaddr_read(reg_w(R_DI), DATA_BYTE);
 				if(cpu.DF == 0){
-					reg_b(R_AL) += DATA_BYTE;
+					reg_w(R_DI) += DATA_BYTE;
 				}else
-					reg_b(R_AL) -= DATA_BYTE;
+					reg_w(R_DI) -= DATA_BYTE;
 			}else if(DATA_BYTE == 2){
-				swaddr_write(REG(R_AX), DATA_BYTE, swaddr_read(reg_w(R_SI), 4));
+				swaddr_write(REG(R_AX), DATA_BYTE, swaddr_read(reg_w(R_DI), 4));
 				if(cpu.DF == 0){
-					reg_w(R_AX) += DATA_BYTE;
+					reg_w(R_DI) += DATA_BYTE;
 				}else
-					reg_w(R_AX) -= DATA_BYTE;
+					reg_w(R_DI) -= DATA_BYTE;
 			}else{
-				swaddr_write(REG(R_EAX), DATA_BYTE, swaddr_read(reg_w(R_SI), 4));
+				swaddr_write(REG(R_EAX), DATA_BYTE, swaddr_read(reg_w(R_DI), 4));
 				if(cpu.DF == 0){
-					reg_l(R_EAX) += DATA_BYTE;
+					reg_w(R_DI) += DATA_BYTE;
 				}else
-					reg_l(R_EAX) -= DATA_BYTE;
+					reg_w(R_DI) -= DATA_BYTE;
 			}
 	}else{
 		if(DATA_BYTE == 1){
-				swaddr_write(REG(R_AL), DATA_BYTE, swaddr_read(reg_l(R_ESI), 4));
+				swaddr_write(REG(R_AL), DATA_BYTE, swaddr_read(reg_l(R_EDI), 4));
 				if(cpu.DF == 0){
-					reg_b(R_AL) += DATA_BYTE;
+					reg_l(R_EDI) += DATA_BYTE;
 				}else
-					reg_b(R_AL) -= DATA_BYTE;
+					reg_l(R_EDI) -= DATA_BYTE;
 			}else if(DATA_BYTE == 2){
-				swaddr_write(REG(R_AX), DATA_BYTE, swaddr_read(reg_l(R_ESI), 4));
+				swaddr_write(REG(R_AX), DATA_BYTE, swaddr_read(reg_l(R_EDI), 4));
 				if(cpu.DF == 0){
-					reg_w(R_AX) += DATA_BYTE;
+					reg_l(R_EDI) += DATA_BYTE;
 				}else
-					reg_w(R_AX) -= DATA_BYTE;
+					reg_l(R_EDI) -= DATA_BYTE;
 			}else{
-				swaddr_write(REG(R_EAX), DATA_BYTE, swaddr_read(reg_l(R_ESI), 4));
+				swaddr_write(REG(R_EAX), DATA_BYTE, swaddr_read(reg_l(R_EDI), 4));
 				if(cpu.DF == 0){
-					reg_l(R_EAX) += DATA_BYTE;
+					reg_l(R_EDI) += DATA_BYTE;
 				}else
-					reg_l(R_EAX) -= DATA_BYTE;
+					reg_l(R_EDI) -= DATA_BYTE;
 			}
 	}
-	print_asm("scas" str(SUFFIX) "0x%X 0x%X",swaddr_read(reg_l(R_ESI), 4),REG(R_AL));
+	print_asm("lods" str(SUFFIX) "0x%X 0x%X",swaddr_read(reg_l(R_EDI), 4),REG(R_AL));
 	//print_asm("mov 0x%X 0x%X", op_src->val, cpu.eax);
 	return 1;
 }
