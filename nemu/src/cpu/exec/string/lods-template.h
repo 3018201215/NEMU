10559ff1acfcd429a1a5fd69edfd44388223fde6
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 03:07:42 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:07:42 up 4 days, 19:13,  1 user,  load average: 0.67, 0.81, 0.78 2116c7e07cba37bf5c1d19fb7049a457ecad0903

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -5,7 +5,7 @@
 make_helper(concat(lods_, SUFFIX)) {
 	if(ops_decoded.is_operand_size_16){
 			if(DATA_BYTE == 1){
-				reg_b(R_AL) = swaddr_read(reg_w(R_SI), DATA_BYTE);
+				reg_b(R_AL) = MEM_R(reg_w(R_SI));
 				if(cpu.DF == 0){
 					reg_w(R_SI) += DATA_BYTE;
 				}else
@@ -25,7 +25,7 @@ make_helper(concat(lods_, SUFFIX)) {
 			}
 	}else{
 		if(DATA_BYTE == 1){
-				reg_b(R_AL) = swaddr_read(reg_w(R_SI), DATA_BYTE);
+				reg_b(R_AL) = MEM_R(reg_w(R_SI));
 				if(cpu.DF == 0){
 					reg_l(R_ESI) += DATA_BYTE;
 				}else
@@ -44,7 +44,7 @@ make_helper(concat(lods_, SUFFIX)) {
 					reg_l(R_ESI) -= DATA_BYTE;
 			}
 	}
-	print_asm("lods" str(SUFFIX) "0x%X 0x%X",swaddr_read(reg_l(R_ESI), DATA_BYTE),reg_b(R_AL));
+	print_asm("lods" str(SUFFIX) "0x%X 0x%X",MEM_R(reg_l(R_ESI)),reg_b(R_AL));
 	//print_asm("mov 0x%X 0x%X", op_src->val, cpu.eax);
 	return 1;
 }
