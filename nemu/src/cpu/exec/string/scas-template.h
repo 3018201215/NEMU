1f3fb2fc88d4aeea3bbd45ae908645f49a854d08
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 16:57:26 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:57:26 up 7 days,  9:02,  1 user,  load average: 0.67, 0.72, 0.63 7165a11c2b2f84142740a4a1bc41f4f448ac62b

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -6,70 +6,93 @@ make_helper(concat(scas_, SUFFIX)) {
 	DATA_TYPE result;
 	int a, b;
 	int len = (DATA_BYTE << 3) - 1;
-	if(ops_decoded.is_operand_size_16){
-			if(DATA_BYTE == 1){
-				swaddr_t val = swaddr_read(reg_w(R_DI), DATA_BYTE);
-				if(cpu.DF == 0){
-					reg_w(R_DI) += DATA_BYTE;
-				}else
-					reg_w(R_DI) -= DATA_BYTE;
-				result = reg_b(R_AL) - val;
-				cpu.CF = reg_b(R_AL) < val;
-				b = reg_b(R_AL) >> len;
-				a = val >> len;
-			}else if(DATA_BYTE == 2){
-				swaddr_t val =  swaddr_read(reg_w(R_DI), DATA_BYTE);
-				if(cpu.DF == 0){
-					reg_w(R_DI) += DATA_BYTE;
-				}else
-					reg_w(R_DI) -= DATA_BYTE;
-				result = reg_w(R_AX) - val;
-				cpu.CF = reg_w(R_AX) < val;
-				b = reg_w(R_AX) >> len;
-				a = val >> len;
-			}else{
-				swaddr_t val =  swaddr_read(reg_w(R_DI), DATA_BYTE);
-				if(cpu.DF == 0){
-					reg_w(R_DI) += DATA_BYTE;
-				}else
-					reg_w(R_DI) -= DATA_BYTE;
-				result = reg_l(R_EAX) - val;
-				cpu.CF = reg_l(R_EAX) < val;
-				b = reg_l(R_EAX) >> len;
-				a = val >> len;
-			}
+	// if(ops_decoded.is_operand_size_16){
+	// 		if(DATA_BYTE == 1){
+	// 			swaddr_t val = swaddr_read(reg_w(R_DI), DATA_BYTE);
+	// 			if(cpu.DF == 0){
+	// 				reg_w(R_DI) += DATA_BYTE;
+	// 			}else
+	// 				reg_w(R_DI) -= DATA_BYTE;
+	// 			result = reg_b(R_AL) - val;
+	// 			cpu.CF = reg_b(R_AL) < val;
+	// 			b = reg_b(R_AL) >> len;
+	// 			a = val >> len;
+	// 		}else if(DATA_BYTE == 2){
+	// 			swaddr_t val =  swaddr_read(reg_w(R_DI), DATA_BYTE);
+	// 			if(cpu.DF == 0){
+	// 				reg_w(R_DI) += DATA_BYTE;
+	// 			}else
+	// 				reg_w(R_DI) -= DATA_BYTE;
+	// 			result = reg_w(R_AX) - val;
+	// 			cpu.CF = reg_w(R_AX) < val;
+	// 			b = reg_w(R_AX) >> len;
+	// 			a = val >> len;
+	// 		}else{
+	// 			swaddr_t val =  swaddr_read(reg_w(R_DI), DATA_BYTE);
+	// 			if(cpu.DF == 0){
+	// 				reg_w(R_DI) += DATA_BYTE;
+	// 			}else
+	// 				reg_w(R_DI) -= DATA_BYTE;
+	// 			result = reg_l(R_EAX) - val;
+	// 			cpu.CF = reg_l(R_EAX) < val;
+	// 			b = reg_l(R_EAX) >> len;
+	// 			a = val >> len;
+	// 		}
+	// }else{
+	// 	if(DATA_BYTE == 1){
+	// 			swaddr_t val = swaddr_read(reg_l(R_EDI), DATA_BYTE);
+	// 			if(cpu.DF == 0){
+	// 				reg_l(R_EDI) += DATA_BYTE;
+	// 			}else
+	// 				reg_l(R_EDI) -= DATA_BYTE;
+	// 			result = reg_b(R_AL) - val;
+	// 			cpu.CF = reg_b(R_AL) < val;
+	// 			b = reg_b(R_AL) >> len;
+	// 			a = val >> len;
+	// 		}else if(DATA_BYTE == 2){
+	// 			swaddr_t val =  swaddr_read(reg_l(R_EDI), DATA_BYTE);
+	// 			if(cpu.DF == 0){
+	// 				reg_l(R_EDI) += DATA_BYTE;
+	// 			}else
+	// 				reg_l(R_EDI) -= DATA_BYTE;
+	// 			result = reg_w(R_AX) - val;
+	// 			cpu.CF = reg_w(R_AX) < val;
+	// 			b = reg_w(R_AX) >> len;
+	// 			a = val >> len;
+	// 		}else{
+	// 			swaddr_t val =  swaddr_read(reg_l(R_EDI), DATA_BYTE);
+	// 			if(cpu.DF == 0){
+	// 				reg_l(R_EDI) += DATA_BYTE;
+	// 			}else
+	// 				reg_l(R_EDI) -= DATA_BYTE;
+	// 			result = reg_l(R_EAX) - val;
+	// 			cpu.CF = reg_l(R_EAX) < val;
+	// 			b = reg_l(R_EAX) >> len;
+	// 			a = val >> len;
+	// 		}
+	// }
+	if(DATA_BYTE == 2){
+		swaddr_t val = swaddr_read(REG(R_DI), DATA_BYTE);
+		result = REG(R_AX) - val;
+		if(cpu.DF == 0){
+			reg_w(R_DI) += DATA_BYTE;
+		}else
+			reg_w(R_DI) -= DATA_BYTE;
+		a = val >> len;
+		b = REG(R_AX) >> len;
 	}else{
+		swaddr_t val = swaddr_read(REG(R_EDI), DATA_BYTE);
+		DATA_TYPE val1 = REG(R_EAX);
 		if(DATA_BYTE == 1){
-				swaddr_t val = swaddr_read(reg_l(R_EDI), DATA_BYTE);
-				if(cpu.DF == 0){
-					reg_l(R_EDI) += DATA_BYTE;
-				}else
-					reg_l(R_EDI) -= DATA_BYTE;
-				result = reg_b(R_AL) - val;
-				cpu.CF = reg_b(R_AL) < val;
-				b = reg_b(R_AL) >> len;
-				a = val >> len;
-			}else if(DATA_BYTE == 2){
-				swaddr_t val =  swaddr_read(reg_l(R_EDI), DATA_BYTE);
-				if(cpu.DF == 0){
-					reg_l(R_EDI) += DATA_BYTE;
-				}else
-					reg_l(R_EDI) -= DATA_BYTE;
-				result = reg_w(R_AX) - val;
-				cpu.CF = reg_w(R_AX) < val;
-				b = reg_w(R_AX) >> len;
-				a = val >> len;
-			}else{
-				swaddr_t val =  swaddr_read(reg_l(R_EDI), DATA_BYTE);
-				if(cpu.DF == 0){
-					reg_l(R_EDI) += DATA_BYTE;
-				}else
-					reg_l(R_EDI) -= DATA_BYTE;
-				result = reg_l(R_EAX) - val;
-				cpu.CF = reg_l(R_EAX) < val;
-				b = reg_l(R_EAX) >> len;
-				a = val >> len;
-			}
+			val1 &= 0xff;
+		}
+		result = val1 - val;
+		if(cpu.DF == 0){
+			reg_l(R_EDI) += DATA_BYTE;
+		}else
+			reg_l(R_EDI) -= DATA_BYTE;
+		a = val1 >> len;
+		b = val >> len;
 	}
 	cpu.SF = result >> len;
 	cpu.OF = (a != b && a == cpu.SF);
@@ -82,7 +105,7 @@ make_helper(concat(scas_, SUFFIX)) {
 	if(result == 0){
 		cpu.ZF = 1;
 	}else cpu.ZF = 0;
-	print_asm("scas" str(SUFFIX) "%d 0x%X %d",swaddr_read(reg_l(R_EDI), DATA_BYTE),reg_b(R_AL), result);
+	print_asm("scas" str(SUFFIX) " 0x%X 0x%X %d",swaddr_read(reg_l(R_EDI), DATA_BYTE),reg_l(R_EAX), result);
 	//print_asm("mov 0x%X 0x%X", op_src->val, cpu.eax);
 	return 1;
 }
