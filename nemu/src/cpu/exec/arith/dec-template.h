26835cdce7a369581291b3d44fca969dcd1bc75d
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 16:37:41 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:37:41 up 7 days,  8:43,  1 user,  load average: 0.40, 0.45, 0.26 76cabc8889072f1ee2da95bd9345853eb2b8a5bc

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -7,8 +7,18 @@ static void do_execute () {
 	OPERAND_W(op_src, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF = op_src->val < 1;
+	int a = op_src->val >> len;
+	cpu.ZF = !result;
+	cpu.SF = result >> len;
+	cpu.OF = (a != 0 && 0 == cpu.SF);
+	DATA_TYPE s = result;
+	int i;	
+	for(i=1; i<8; i++){
+		s ^= result >> i;	
+	}
+	cpu.PF = !(s & 1);
 	print_asm_template1();
 }
 
