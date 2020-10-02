1324a4d6188f43e3d8d596ae30895f761cc9fb53
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 14:11:52 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:11:52 up 7 days,  6:17,  1 user,  load average: 1.25, 0.98, 0.67 753175bcace18133bb3da55340983b3e9cca872a

---
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -0,0 +1,38 @@
+#include "cpu/exec/template-start.h"
+
+#define instr stos
+
+make_helper(concat(stos_, SUFFIX)) {
+	if(ops_decoded.is_operand_size_16){
+		swaddr_t val = swaddr_read(reg_w(R_DI), DATA_BYTE);
+		DATA_TYPE val1 = REG(R_EAX);
+		if(DATA_BYTE == 1)
+			val1 &= 0xff;
+		else if(DATA_BYTE == 2)
+			val1 &= 0xffff;
+		swaddr_write(val, val1, DATA_BYTE);
+		if(cpu.DF == 0){
+			reg_w(R_DI) += DATA_BYTE;
+		}else
+			reg_w(R_DI) -= DATA_BYTE;
+	}else{
+		swaddr_t val = swaddr_read(reg_l(R_EDI), DATA_BYTE);
+		DATA_TYPE val1 = REG(R_EAX);
+		if(DATA_BYTE == 1)
+			val1 &= 0xff;
+		else if(DATA_BYTE == 2)
+			val1 &= 0xffff;
+		swaddr_write(val, val1, DATA_BYTE);
+		if(cpu.DF == 0){
+			reg_l(R_EDI) += DATA_BYTE;
+		}else
+			reg_l(R_EDI) -= DATA_BYTE;
+	}
+	print_asm("stos" str(SUFFIX));
+	//print_asm("mov 0x%X 0x%X", op_src->val, cpu.eax);
+	return 1;
+}
+
+
+
+#include "cpu/exec/template-end.h"
