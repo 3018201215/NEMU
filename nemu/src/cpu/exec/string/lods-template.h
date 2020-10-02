f1f047dd6b59dde1a5ccadb321afd6341dc9e1ec
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 17:19:33 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:19:33 up 7 days,  9:24,  1 user,  load average: 0.71, 0.66, 0.69 8e29ca31e2933a54ca09c541437caf54cbb2c38a

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -4,19 +4,19 @@
 
 make_helper(concat(lods_, SUFFIX)) {
 	if(DATA_BYTE == 2){
-		REG(R_EAX) = swaddr_read(reg_w(R_SI), DATA_BYTE);
+		reg_w(R_AX) = swaddr_read(reg_w(R_SI), DATA_BYTE);
 		if(cpu.DF == 0){
 			reg_w(R_SI) += DATA_BYTE;
 		}else
 			reg_w(R_SI) -= DATA_BYTE;
 	}else{
-		REG(R_EAX) = swaddr_read(reg_l(R_ESI), DATA_BYTE);
+		reg_l(R_EAX) = swaddr_read(reg_l(R_ESI), DATA_BYTE);
 		if(cpu.DF == 0){
 			reg_l(R_ESI) += DATA_BYTE;
 		}else
 			reg_l(R_ESI) -= DATA_BYTE;
 	}
-	print_asm("lods" str(SUFFIX) "0x%X 0x%X",REG(R_ESI),REG(R_EAX));
+	print_asm("lods" str(SUFFIX) " 0x%X 0x%X",REG(R_ESI),REG(R_EAX));
 	//print_asm("mov 0x%X 0x%X", op_src->val, cpu.eax);
 	return 1;
 }
