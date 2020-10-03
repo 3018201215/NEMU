3de14485b817f8ccf6170201cd78dd78fb7efaaf
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 13:22:14 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:22:14 up 8 days,  5:27,  1 user,  load average: 0.99, 0.94, 0.68 16969c75109075da5ae506c8d33cf3099f9967e1

--- nemu/src/cpu/exec/logic/shr-template.h
+++ nemu/src/cpu/exec/logic/shr-template.h
@@ -5,11 +5,18 @@
 static void do_execute () {
 	DATA_TYPE src = op_src->val;
 	DATA_TYPE dest = op_dest->val;
-
+	int len = (DATA_BYTE << 3) - 1;
 	uint8_t count = src & 0x1f;
+	cpu.CF = !!(dest & (1 << (count - 1)));
 	dest >>= count;
 	OPERAND_W(op_dest, dest);
-
+	cpu.OF = 0;
+	cpu.SF = dest >> len;
+	cpu.ZF = !dest;
+	dest ^= (dest >> 4);
+	dest ^= (dest >> 2);
+	dest ^= (dest >> 1);
+	cpu.PF = !(dest & 1);
 	/* There is no need to update EFLAGS, since no other instructions 
 	 * in PA will test the flags updated by this instruction.
 	 */
