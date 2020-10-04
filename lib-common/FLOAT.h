42707c6ce62ba97dca3202479559d9ff109b89be
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 12:46:43 +0000

    >  run 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:46:43 up 9 days,  4:52,  1 user,  load average: 0.58, 0.81, 0.83 b14c90bde62235a1296f931c7ba4cd158aa4a489

--- lib-common/FLOAT.h
+++ lib-common/FLOAT.h
@@ -30,8 +30,8 @@ FLOAT f2F(float);
 FLOAT F_mul_F(FLOAT, FLOAT);
 FLOAT F_div_F(FLOAT, FLOAT);
 FLOAT Fabs(FLOAT);
-// FLOAT sqrt(FLOAT);
-// FLOAT pow(FLOAT, FLOAT);
+FLOAT sqrt(FLOAT);
+FLOAT pow(FLOAT, FLOAT);
 
 // used when calling printf/sprintf to format a FLOAT argument
 #define FLOAT_ARG(f) (long long)f
--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -80,27 +80,27 @@ FLOAT Fabs(FLOAT a) {
 
 /* Functions below are already implemented */
 
-// FLOAT sqrt(FLOAT x) {
-// 	FLOAT dt, t = int2F(2);
+FLOAT sqrt(FLOAT x) {
+	FLOAT dt, t = int2F(2);
 
-// 	do {
-// 		dt = F_div_int((F_div_F(x, t) - t), 2);
-// 		t += dt;
-// 	} while(Fabs(dt) > f2F(1e-4));
+	do {
+		dt = F_div_int((F_div_F(x, t) - t), 2);
+		t += dt;
+	} while(Fabs(dt) > f2F(1e-4));
 
-// 	return t;
-// }
+	return t;
+}
 
-// FLOAT pow(FLOAT x, FLOAT y) {
-// 	/* we only compute x^0.333 */
-// 	FLOAT t2, dt, t = int2F(2);
+FLOAT pow(FLOAT x, FLOAT y) {
+	/* we only compute x^0.333 */
+	FLOAT t2, dt, t = int2F(2);
 
-// 	do {
-// 		t2 = F_mul_F(t, t);
-// 		dt = (F_div_F(x, t2) - t) / 3;
-// 		t += dt;
-// 	} while(Fabs(dt) > f2F(1e-4));
+	do {
+		t2 = F_mul_F(t, t);
+		dt = (F_div_F(x, t2) - t) / 3;
+		t += dt;
+	} while(Fabs(dt) > f2F(1e-4));
 
-// 	return t;
-// }
+	return t;
+}
 
