6c472d08139fcc95fea6381c8228ee0713ffadd6
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 16:04:45 +0000

    >  compile NEMU 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:04:45 up 9 days,  8:10,  1 user,  load average: 0.63, 0.77, 0.79 dae0be5c52dee1cbb6283ddd36f8b1687d39b309

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -3,11 +3,11 @@
 #define instr pop
 
 static void do_execute(){
-	OPERAND_W(op_src, swaddr_read(reg_l(R_ESP), DATA_BYTE));
+	OPERAND_W(op_src,  MEM_R(REG(R_ESP)));
 	MEM_W(reg_l(R_ESP), 0);
 	reg_l(R_ESP) += DATA_BYTE;
-	print_asm_template1();
-	//print_asm("push" str(SUFFIX) " sp:0x%X", REG(R_ESP));
+	//print_asm_template1();
+	print_asm("push" str(SUFFIX) " bx:0x%X", REG(R_EBX));
 }
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
