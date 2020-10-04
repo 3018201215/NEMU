793ac92044e62541ac67aada60b49586ef0776ff
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 07:59:56 +0000

    >  run 3018201215 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:59:56 up 9 days, 5 min,  1 user,  load average: 0.33, 0.49, 0.53 346e2344f6881c39e385aca26c592ec6181ec35

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -6,7 +6,7 @@ char buf[128];
 
 int main() {
 	sprintf(buf, "%d", 0);
-	nemu_assert(strcmp(buf, "0") == 0);
+//	nemu_assert(strcmp(buf, "0") == 0);
 
 //	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 //	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
