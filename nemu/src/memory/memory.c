75326deda8b4abf5dbd51c4602467b3123e96c16
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 05:35:31 +0000

    >  test 3018201215 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:35:31 up  3:41,  1 user,  load average: 0.89, 0.85, 0.83 a7f5e7eb6485f4741fcbe3970ef8c884ec5a71

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,56 +1,82 @@
 #include "common.h"
 #include "burst.h"
-#include "memory/cache.h"
 #include "nemu.h"
-#include <stdlib.h>
-#include "cpu/reg.h"
+#include "memory/cache.h"
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
+
 /* Memory accessing interfaces */
 
+lnaddr_t seg_translate(swaddr_t addr,size_t len,uint8_t sreg) {
+	if(cpu.cr0.protect_enable==0) return addr;
+	Assert(addr+len<cpu.sr[sreg].seg_limit,"OUT LIMIT");
+	return cpu.sr[sreg].base_addr+addr;
+}
+
+hwaddr_t page_translate(lnaddr_t addr,size_t len) {
+	if(cpu.cr0.paging==1&&cpu.cr0.protect_enable==1) {
+		uint32_t dir = addr >> 22; 
+		uint32_t page = (addr>>12)&0x3ff;
+		uint32_t offset = addr&0xfff;
+		Page_entry dir_1,page_1;
+		dir_1.val = hwaddr_read((cpu.cr3.page_directory_base<<12)+(dir<<2),4);
+		Assert(dir_1.p,"Invalid page");
+		page_1.val = hwaddr_read((dir_1.base<<12)+(page<<2),4);
+		Assert(page_1.p,"Invalid page");
+		hwaddr_t hwaddr = (page_1.base<<12)+offset;
+		//Assert((hwaddr&0xfff)+len==((hwaddr+len)&0xfff),"Fatal Error");
+		return hwaddr;
+	}
+	else {
+		return addr;
+	}
+}
+
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	// int l1 = read_cache(addr);
-	// uint32_t offset = addr & (Cache_block_size - 1);
-	// uint8_t ret[BURST_LEN * 2];
-	// memset(ret, 0, sizeof(ret));
-	// if(offset + len > Cache_block_size){
-	// 	int l2 = read_cache(addr + Cache_block_size - offset);
-	// 	memcpy(ret, cache[l1].data + offset, Cache_block_size - offset);
-	// 	memcpy(ret+Cache_block_size-offset, cache[l2].data, len-(Cache_block_size-offset));
-	// }else{
-	// 	memcpy(ret, cache[l1].data+offset, len);
-	// }
-	// int tmp = 0;
-	// uint32_t ans = unalign_rw(ret + tmp, 4) & (~0u >> ((4 - len) << 3));
-	// return ans;
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	int id = cache_read(addr);
+	uint32_t offset = addr&(CACHE_BLOCK_SIZE-1);
+	uint8_t tmp[2*BURST_LEN];
+	if(offset+len>CACHE_BLOCK_SIZE) {
+		int id2 = cache_read(addr+CACHE_BLOCK_SIZE-offset);	
+		memcpy(tmp,cache[id].data+offset,CACHE_BLOCK_SIZE-offset);
+		memcpy(tmp+CACHE_BLOCK_SIZE-offset,cache[id2].data,len-(CACHE_BLOCK_SIZE-offset));
+	}
+	else {
+		memcpy(tmp,cache[id].data+offset,len);
+	}
+	int zero=0;
+	uint32_t ans = unalign_rw(tmp+zero,4)&(~0u >> ((4 - len) << 3));
+	return ans;
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	// write_cache(addr, len, data);
-	dram_write(addr, len, data);
+	cache_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
-	return hwaddr_read(addr, len);
+	hwaddr_t hwaddr = page_translate(addr,len);	
+	return hwaddr_read(hwaddr, len);
 }
 
 void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
-	hwaddr_write(addr, len, data);
+	hwaddr_t hwaddr = page_translate(addr,len);	
+	hwaddr_write(hwaddr, len, data);
 }
 
-uint32_t swaddr_read(swaddr_t addr, size_t len) {
+uint32_t swaddr_read(swaddr_t addr, size_t len,uint8_t sreg) {
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
-	return lnaddr_read(addr, len);
+	lnaddr_t lnaddr = seg_translate(addr,len,sreg);
+	return lnaddr_read(lnaddr, len);
 }
 
-void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
+void swaddr_write(swaddr_t addr, size_t len, uint32_t data,uint8_t sreg) {
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
-	lnaddr_write(addr, len, data);
+	lnaddr_t lnaddr = seg_translate(addr,len,sreg);
+	lnaddr_write(lnaddr, len, data);
 }
 
